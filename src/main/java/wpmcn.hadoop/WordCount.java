11401237fc6cafcc3a5279f09617c571a7dd9ce0
W.P. McNeill <billmcn@gmail.com>
2012-01-07 14:49:26 -0800

    Write Word Count
    A very simple word count application.

--- src/main/java/wpmcn.hadoop/WordCount.java
+++ src/main/java/wpmcn.hadoop/WordCount.java
@@ -1,7 +1,75 @@
 package wpmcn.hadoop;
 
-public class WordCount {
-   public static void main(String[] args) {
-      System.out.println("Hello World!");
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.IOException;
+
+public class WordCount extends Configured implements Tool {
+
+   static public class WordCountMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+      final private static LongWritable ONE = new LongWritable(1);
+      private Text tokenValue = new Text();
+
+      @Override
+      protected void map(LongWritable offset, Text text, Context context) throws IOException, InterruptedException {
+         for (String token : text.toString().split("\\s+")) {
+            tokenValue.set(token);
+            context.write(tokenValue, ONE);
+         }
+      }
+   }
+
+   static public class WordCountReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
+      private LongWritable total = new LongWritable();
+
+      @Override
+      protected void reduce(Text token, Iterable<LongWritable> counts, Context context)
+            throws IOException, InterruptedException {
+         long n = 0;
+         for (LongWritable count : counts)
+            n += count.get();
+         total.set(n);
+         context.write(token, total);
+      }
+   }
+
+   public int run(String[] args) throws Exception {
+      Configuration configuration = getConf();
+
+      Job job = new Job(configuration, "Word Count");
+      job.setJarByClass(WordCount.class);
+
+      int n = args.length;
+      if (n > 0)
+         TextInputFormat.addInputPath(job, new Path(args[0]));
+      if (n > 1)
+         SequenceFileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+      job.setMapperClass(WordCountMapper.class);
+      job.setCombinerClass(WordCountReducer.class);
+      job.setReducerClass(WordCountReducer.class);
+
+      job.setInputFormatClass(TextInputFormat.class);
+      job.setOutputFormatClass(SequenceFileOutputFormat.class);
+
+      job.setOutputKeyClass(Text.class);
+      job.setOutputValueClass(LongWritable.class);
+
+      return job.waitForCompletion(true) ? 0 : -1;
+   }
+
+   public static void main(String[] args) throws Exception {
+      System.exit(ToolRunner.run(new WordCount(), args));
    }
 }
